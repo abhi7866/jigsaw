94912b1cf0b93b475bd6ebf33066e5aa0f9813e9
ABHISHEK RANJAN <73189797+abhi7866@users.noreply.github.com>
2021-05-17 14:33:50 +0530

    Add files via upload

---
+++ js/DropPuzzlePieceUtils.js
@@ -0,0 +1,55 @@
+function DropPuzzlePieceUtils(){}
+
+DropPuzzlePieceUtils.prototype.testSocketColision = function(socket){
+
+    if(socket != null && socket.isJoined == false && socket.isColliding()){
+
+        this.fitSocket(socket);
+
+        socket.isJoined = true;
+
+        socket.slave.isJoined = true;
+
+        currentSocketsCount += 2;
+
+    }
+}
+
+DropPuzzlePieceUtils.prototype.fitSocket = function(socket){
+
+    var diffX = socket.slave.getCenterX() - socket.getCenterX();
+
+    var diffY = socket.slave.getCenterY() - socket.getCenterY();
+
+    var dropLayer = ComponentUtils.getComponent(layer, "DROP_PUZZLE_PIECE_LAYER_COMPONENT");
+
+    dropLayer.resetMovedObjects();
+
+    socket.parentPiece.addMoveAll(diffX, diffY, true);
+}
+
+DropPuzzlePieceUtils.prototype.onDropSockets = function(gameObject){
+
+    this.testSocketColision(gameObject.leftSocket);
+
+    this.testSocketColision(gameObject.rightSocket);
+
+    this.testSocketColision(gameObject.topSocket);
+
+    this.testSocketColision(gameObject.bottomSocket);
+}
+
+DropPuzzlePieceUtils.prototype.onDropGameObject = function(gameObject){
+
+	for(var i=0; i<columns; i++){
+
+    	for(var j=0; j<rows; j++){
+
+    		var pieceObj = pieces[i][j];
+
+			this.onDropSockets(pieceObj);
+
+		}
+
+	}
+}
---
+++ js/PaperJsRenderComponent.js
@@ -0,0 +1,43 @@
+function PaperJsRenderComponent(){}
+
+PaperJsRenderComponent.prototype = new Component();
+
+PaperJsRenderComponent.prototype.finished = false;
+
+PaperJsRenderComponent.prototype.onUpdate = function(delta){
+
+	if(currentSocketsCount == socketsCount && this.finished == false){
+
+		this.finished = true;
+
+		BootstrapDialog.show({
+
+            title: 'Congratulations!',
+
+            message: 'You completed the Puzzle.'
+ 
+       });
+
+	}
+}
+
+PaperJsRenderComponent.prototype.onRender = function(context){
+
+	paper.view.draw();
+}
+
+PaperJsRenderComponent.prototype.getSystems = function(){
+
+	var systems = new Array();
+
+	systems = ArrayUtils.addElement(systems, RenderSystem);
+
+	systems = ArrayUtils.addElement(systems, LogicSystem);
+
+	return systems;
+}
+
+PaperJsRenderComponent.prototype.getTag = function(){
+
+	return "PAPER_JS_RENDER_COMPONENT";
+}
